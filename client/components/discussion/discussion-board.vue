f8abf83248a56af1bac96dc6913332df40d27c12
manyimy <wong.manyi19@gmail.com>
2020-09-06 17:16:59 +0800

    Debugged Delete Discussion

--- client/components/discussion/discussion-board.vue
+++ client/components/discussion/discussion-board.vue
@@ -10,7 +10,7 @@
               size="sm"
               class="float-right"
               variant="outline-danger"
-              @click="onDelete(i)"
+              @click="onDelete(discussion.id)"
             >
               Delete
             </b-button>
