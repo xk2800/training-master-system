15a32231c2aa588b54a004250109328d306055d9
SJ K <shijingkoh@Outlook.com>
2020-09-07 20:24:57 +0800

    update detail view UI for admin

--- server/routes/get/admin.route.js
+++ server/routes/get/admin.route.js
@@ -3,49 +3,49 @@
  * request:
  *   - token: access token AND
  *   - id: user id OR
- *   - trainer_id: trainer_id
+ *   - admin_id: admin id
  * response:
- *   - id: the trainer id OR
- *   - userId: the trainer's user id AND
- *   - department: user department
+ *   - id: the admin id OR
+ *   - userId: the admin's user id AND
+ *   - title: admin's title
  *   - status: 0 = success, 1 = access denied, 2 = error occurs
  */
 
 import verifier from '../../utils/token-verifier.js'
 import { models } from '../../db.js'
 
-const { Trainer } = models
+const { Admin } = models
 
 export default (req, res) => {
   
-  const { token, id, trainer_id } = req.query
+  const { token, id, admin_id } = req.query
 
   verifier(token, (valid) => {
     if (!valid) return res.status(200).json({ status: 1 })
     if(id !== undefined)
-      Trainer
+      Admin
         .findOne({ where: { userId: id } })
         .then((model) => {
           if (!model) return res.status(200).json({ status: 1 })
-          const { id, department } = model
+          const { id, title } = model
           res.status(200).json({
             status: 0,
-            id, department
+            id, title
           })
         })
         .catch((error) => {
           console.log(error)
           return res.status(500).json({ status: 2 })
         })
-    else if(trainer_id !== undefined)
-      Trainer
+    else if(admin_id !== undefined)
+      Admin
         .findOne({ where: { id: id } })
         .then((model) => {
           if (!model) return res.status(200).json({ status: 1 })
-          const { userId, department } = model
+          const { userId, title } = model
           res.status(200).json({
             status: 0,
-            userId, department
+            userId, title
           })
         })
         .catch((error) => {
